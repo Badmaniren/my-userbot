5b49b956817d5b178ac1cc03690aab8f0ac0c8ed
Baddy <Kilkmenc@mail.ru>
2026-09-10 23:03:41 +0500

    Delete victim.py

--- victim.py
+++
@@ -1,9 +0,0 @@
-def solve_task(a, b):
-    def parse(val):
-        if isinstance(val, str):
-            val = val.strip()
-            if not val:
-                raise ValueError()
-        return float(val)
-    res = parse(a) + parse(b)
-    return int(res) if res.is_integer() else res

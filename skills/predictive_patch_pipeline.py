57b10f086d2be0cd1e6c5487dfddd3f2249cfbff
Baddy <Kilkmenc@mail.ru>
2026-09-19 21:01:43 +0500

    Реализация #3 для predictive_patch_pipeline

--- skills/predictive_patch_pipeline.py
+++ skills/predictive_patch_pipeline.py
@@ -18,8 +18,14 @@ class PredictivePatchPipeline:
         else:
             module_name = kwargs.get("module_name")
 
-        telemetry_res = self.bridge.run_vulnerability_telemetry_cycle(*args, **kwargs)
-        patch_res = self.applier.run_preventive_cycle(module_name)
+        try:
+            telemetry_res = self.bridge.run_vulnerability_telemetry_cycle(*args, **kwargs)
+            patch_res = self.applier.run_preventive_cycle(module_name)
+        except Exception as e:
+            if isinstance(e, PredictivePatchPipelineError):
+                raise e
+            raise PredictivePatchPipelineError(str(e)) from e
+
         return {
             "telemetry_cycle": telemetry_res,
             "preventive_cycle": patch_res,
@@ -27,8 +33,14 @@ class PredictivePatchPipeline:
         }
 
     def process_stream_and_apply_patches(self, module_name, stream_io, path):
-        stream_result = self.bridge.process_stream_and_telemetry(stream_io, path)
-        prevent_result = self.applier.prevent_failures_from_stream(module_name, stream_io)
+        try:
+            stream_result = self.bridge.process_stream_and_telemetry(stream_io, path)
+            prevent_result = self.applier.prevent_failures_from_stream(module_name, stream_io)
+        except Exception as e:
+            if isinstance(e, PredictivePatchPipelineError):
+                raise e
+            raise PredictivePatchPipelineError(str(e)) from e
+
         return {
             "stream_processing": stream_result,
             "preventive_stream_result": prevent_result,
@@ -37,8 +49,14 @@ class PredictivePatchPipeline:
 
     def execute_full_pipeline_flow(self, **kwargs):
         module_name = kwargs.get("module_name")
-        bridge_output = self.bridge.execute_bridge_pipeline(**kwargs)
-        apply_output = self.applier.apply_preventive_patches(module_name)
+        try:
+            bridge_output = self.bridge.execute_bridge_pipeline(**kwargs)
+            apply_output = self.applier.apply_preventive_patches(module_name)
+        except Exception as e:
+            if isinstance(e, PredictivePatchPipelineError):
+                raise e
+            raise PredictivePatchPipelineError(str(e)) from e
+
         return {
             "bridge_pipeline": bridge_output,
             "apply_preventive_patches": apply_output,
@@ -46,8 +64,14 @@ class PredictivePatchPipeline:
         }
 
     def export_and_apply_pipeline(self, payload, path, module_name):
-        self.bridge.export_risk_and_vulnerability_report(payload, path)
-        process_output = self.applier.process_module(module_name)
+        try:
+            self.bridge.export_risk_and_vulnerability_report(payload, path)
+            process_output = self.applier.process_module(module_name)
+        except Exception as e:
+            if isinstance(e, PredictivePatchPipelineError):
+                raise e
+            raise PredictivePatchPipelineError(str(e)) from e
+
         return {
             "process_module": process_output,
             "report_exported": True,

6ba2051fdd47a69ba9e4899ca448868f8813b60c
Baddy <Kilkmenc@mail.ru>
2026-09-22 14:31:00 +0500

    Реализация #1 для market_portfolio_audit_compliance_hub

---
+++ skills/market_portfolio_audit_compliance_hub.py
@@ -0,0 +1,45 @@
+from skills.db_storage import MarketParser
+from skills.market_portfolio_audit_log_exporter import PortfolioAuditLogExporter
+
+
+class MarketPortfolioAuditComplianceHub:
+    def __init__(self, storage_file=None, db_storage=None, audit_exporter=None):
+        if db_storage is not None:
+            self.db_storage = db_storage
+        else:
+            self.db_storage = MarketParser(storage_file)
+
+        if audit_exporter is not None:
+            self.audit_exporter = audit_exporter
+        else:
+            self.audit_exporter = PortfolioAuditLogExporter(storage_file)
+
+    def run_compliance_export(self, export_path):
+        return self.audit_exporter.export_audit_logs(export_path)
+
+    def check_compliance_integrity(self):
+        return self.audit_exporter.verify_log_integrity()
+
+    def fetch_compliance_summary(self):
+        return self.audit_exporter.get_audit_stream_summary()
+
+    def process_audit_stream_data(self, export_path, stream):
+        return self.audit_exporter.process_audit_stream(export_path, stream)
+
+    def generate_compliance_log(self, export_path):
+        return self.audit_exporter.generate_audit_log(export_path)
+
+    def audit_fetch_market_price(self, url):
+        return self.db_storage.fetch_price(url)
+
+    def load_historical_audit_data(self, filename):
+        return self.db_storage.load_data(filename)
+
+    def get_audit_stream_summary(self):
+        return self.audit_exporter.get_audit_stream_summary()
+
+    def verify_log_integrity(self):
+        return self.audit_exporter.verify_log_integrity()
+
+    def export_audit_logs(self, export_path):
+        return self.audit_exporter.export_audit_logs(export_path)
